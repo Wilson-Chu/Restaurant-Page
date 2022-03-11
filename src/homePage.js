32dd9c4a5dee4319de57134c7faaddba0187396b
Wilson Chu <wilson1.chu@gmail.com>
2022-03-11 18:30:12 -0500

    setup css and style loaders

--- src/homePage.js
+++ src/homePage.js
@@ -1,3 +1,10 @@
 export default function homePage() {
     console.log('I\'m home!');
+
+    const content = document.querySelector('#content');
+
+    const background = document.createElement('div');
+    background.id = 'background';
+    background.innerHTML = 'Home Page is Here!'
+    content.appendChild(background);
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import homePage from './homePage.js';
+import './styles/homePage.css';
 
 console.log('Hello world!');
 homePage(); // console.log's "I'm home!" - module works!
