6e7983633e00f3e5842ea58d02cd872e5bcdb3d4
Wilson Chu <wilson1.chu@gmail.com>
2022-03-25 21:20:54 -0400

    import sample pictures into menu.js for future use

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,7 @@
+import pizza1 from './assets/pizza1.jpg';
+import pizza2 from './assets/pizza2.jpg';
+import pizza3 from './assets/pizza3.jpg';
+import pizza4 from './assets/pizza4.jpg';
 import './styles/menu.css';
 
 export default function loadMenu() {
