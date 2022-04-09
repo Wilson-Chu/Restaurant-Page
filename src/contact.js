a5ebb6ec4c4094fe03bb8526bd90127732795a52
Wilson Chu <wilson1.chu@gmail.com>
2022-04-08 21:07:37 -0400

    add basic text to start Contact tab

--- src/contact.js
+++ src/contact.js
@@ -7,4 +7,13 @@ export default function loadContact() {
     const background = document.createElement('div');
     background.id = 'background';
     contactPage.appendChild(background);
+
+    const contactContainer = document.createElement('div');
+    contactContainer.id = 'contact-container';
+    background.appendChild(contactContainer);
+
+    const contactTitle = document.createElement('div');
+    contactTitle.id = 'contact-title';
+    contactTitle.textContent = 'Contact Us';
+    contactContainer.appendChild(contactTitle);
 }
